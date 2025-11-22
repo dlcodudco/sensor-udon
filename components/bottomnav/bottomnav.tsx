cbf5b8cb457c0751e5719b1c635ad0b8d34ff9d7
dlcodudco <dlcodudco@jbnu.ac.kr>
2025-11-23 01:11:25 +0900

    feat: 레이아웃 상하단 위치 조절

--- components/bottomnav/bottomnav.tsx
+++ components/bottomnav/bottomnav.tsx
@@ -2,7 +2,7 @@
 
 export default function BottomNav() {
   return (
-    <nav className="fixed bottom-0 left-0 w-full h-16 bg-white border-t flex justify-around items-center">
+    <nav className="fixed bottom-0 left-0 w-full h-16 bg-white border-t flex justify-around items-center pb-[env(safe-area-inset-bottom)]">
       <a href="/sensor">센서</a>
       <a href="/camera">카메라</a>
       <a href="/history">기록</a>
